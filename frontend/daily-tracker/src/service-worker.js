ac28797e409ebe6ad3f17ef60f05105f00e98491
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2026-05-06 17:36:11 +0530

    Update service-worker.js

--- frontend/daily-tracker/src/service-worker.js
+++ frontend/daily-tracker/src/service-worker.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = "my-pwa-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html"
-];
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-unused-expressions */
 
-self.addEventListener("install", (event) => {
+self.__WB_MANIFEST;
+
+const CACHE_NAME = 'daily-tracker-cache-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
@@ -12,7 +14,7 @@ self.addEventListener("install", (event) => {
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);

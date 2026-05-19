53d85cb70fe152a4e801f44911d027c6f224ca36
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2026-05-20 03:30:04 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const cron = require("node-cron");
 const express = require("express");
 const cors = require("cors");
@@ -8,7 +9,7 @@ const trackerRoutes = require("./routes/trackerRoutes");
 const app = express();
 
 connectDB();
-
+console.log("JWT:", process.env.JWT_SECRET)
 cron.schedule("0 0 * * *", async () => {
   console.log("🔄 Midnight Reset Running...");
 
@@ -27,12 +28,12 @@ cron.schedule("0 0 * * *", async () => {
   );
 
   console.log("✅ Daily Progress Reset Completed");
-  });
-app.use(cors({ origin: 'https://your-vercel-url.vercel.app' }));
+});
+app.use(cors());
 app.use(express.json());
 
 app.use("/tracker", trackerRoutes);
 
 app.use("/api", require("./routes/japRoutes"));
-
+app.use("/api/auth", require("./routes/authRoutes"));
 app.listen(5000, () => console.log("Server running on port 5000"));
